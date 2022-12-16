1696386256ae5976440d65eb59b249bba4c96fd6
Btaype <90937585+btaype@users.noreply.github.com>
2022-12-16 18:32:01 -0500

    Add files via upload

---
+++ AutoCompleteEntry/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using dotMorten.Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AutoCompleteEntry
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    { 
+        private List<string> CountriesList;
+        public string datos;
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            GetCountriesFromFile();
+        }
+       
+        
+        void ButtonSubmitClicked(object sender, EventArgs e)
+        {
+            string nombre1 = nombre.Text;
+            string nacionalidad1 = nacionalidad.SelectedItem as string;
+            string telf1 = telf.Text;
+            string correo1 = email.Text;
+            string ocupacion1 = ocupacion.Text;
+            string nivel = datos;
+            
+
+
+            Application.Current.MainPage.Navigation.PushModalAsync(new Page1(nombre1, nacionalidad1,telf1,correo1,ocupacion1,nivel),true);
+        }
+        private void GetCountriesFromFile()
+        {
+            using(var country = typeof(MainPage).Assembly.GetManifestResourceStream("AutoCompleteEntry.Data.Countries.txt"))
+            {   
+
+                CountriesList = new StreamReader(country).ReadToEnd().Split('\n').Select(t => t.Trim()).ToList();
+            }
+        }
+
+        private void contryEntry_TextChanged(object sender, AutoSuggestBoxTextChangedEventArgs e)
+        {
+            AutoSuggestBox input = (AutoSuggestBox)sender;
+
+            input.ItemsSource = GetSuggestions(input.Text);
+        }
+
+        private List<string> GetSuggestions(string input)
+        {
+            return string.IsNullOrWhiteSpace(input) ? null : CountriesList.Where(c => c.StartsWith(input, StringComparison.InvariantCultureIgnoreCase)).ToList();
+        }
+
+        private void contryEntry_QuerySubmitted(object sender, AutoSuggestBoxQuerySubmittedEventArgs e)
+        {
+           
+        }
+         public void seleccion(object sender,EventArgs args)
+        {
+            RadioButton rad = sender as RadioButton;
+            datos = rad.Content as string;
+          
+        }
+    }
+}
---
+++ AutoCompleteEntry/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AutoCompleteEntry
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1(string nombre1,string nacionalidad1,string telf1,string  correo1,string ocupacion1,string nivel1)
+        {
+            InitializeComponent();
+            nombre.Text = nombre1;
+            nacionalidadd.Text = nacionalidadd.Text + nacionalidad1;
+            telf.Text = telf1;
+            correo.Text = correo1;
+            ocupacion.Text = ocupacion1;
+            nivel.Text= nivel.Text+nivel1;
+
+
+
+
+
+        }
+    }
+}
